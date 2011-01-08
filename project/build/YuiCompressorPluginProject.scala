379c9e0991f9b4ab44070d98ddbf53616a866f79
Victor Bilyk <victorbilyk@gmail.com>
2011-01-08 19:23:39 +0600

    broken it for my own needs

--- project/build/YuiCompressorPluginProject.scala
+++ project/build/YuiCompressorPluginProject.scala
@@ -1,6 +1,7 @@
 import sbt._
 class YuiCompressorPluginProject(info: ProjectInfo) extends PluginProject(info) {
-  val yuiCompressor = "com.yahoo.platform.yui" % "yuicompressor" % "2.4.2" % "compile->default"
+  val yuiCompressor = "com.yahoo.platform.yui" % "yuicompressor" % "2.4.4" % "compile->default" from "http://cloud.github.com/downloads/vspy/yuicompressor/yuicompressor-2.4.4.jar"
+  val rhinoYui = "rhino" % "js" % "1.6R7yui" from "http://cloud.github.com/downloads/vspy/rhino1.6R7-yui/js.jar"
   
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
--- src/main/scala/hoffrocket/YuiCompressorPlugin.scala
+++ src/main/scala/hoffrocket/YuiCompressorPlugin.scala
@@ -11,84 +11,85 @@ import java.io.OutputStreamWriter
 import org.mozilla.javascript.ErrorReporter
 import org.mozilla.javascript.EvaluatorException
 
-trait YuiCompressorPlugin extends DefaultWebProject{
-  
-  def yuiSourceFilter = "*.css" | "*.js"
-  def yuiSources = descendents(webappPath, yuiSourceFilter)
-  def yuiTemp = outputPath / "yui-temp"
-  
-  def yuiEncoding = "UTF-8"
-  def yuiNomunge = false
-  def yuiJswarn = false
-  def yuiPreserveAllSemiColons = false
-  def yuiDisableOptimizations = false
-  def lineBreaks = -1
-  
-  def yuiProducts:Iterable[Path] = for (path <- yuiSources.get) yield Path.fromFile(yuiTemp.toString + path.toString.substring(webappPath.toString.length))
-  
-  lazy val yuicompressor = yuicompressorAction 
-  
-  def yuicompressorAction = fileTask(yuiProducts from yuiSources) {
-    val errorReporter = new YuiErrorReporter()
-    for(file <- yuiSources.get){
-        val inFile = new File(file.toString)
-        val in = new InputStreamReader(new FileInputStream(inFile), yuiEncoding)
-		try {
-	        val fileName = file.toString.substring(webappPath.toString.length)
-
-	        val outFile = new File( yuiTemp.toString + fileName)
-
-	        if (!outFile.exists || outFile.lastModified <  inFile.lastModified){
-            
-	            log.info("Compressing: " + fileName)
-	            val extension = fileName.substring(fileName.lastIndexOf("."))
-            
-	            if (!outFile.getParentFile.exists && !outFile.getParentFile.mkdirs) {
-	                throw new Exception( "Cannot create resource output directory: " + outFile.getParentFile() )
-	            }
-	            val out = new OutputStreamWriter(new FileOutputStream(outFile), yuiEncoding)
-	            try {
-	                if (".js".equalsIgnoreCase(extension)) {
-	                    val compressor = new JavaScriptCompressor(in, errorReporter)
-
-	                    compressor.compress(out, lineBreaks, !yuiNomunge, yuiJswarn, yuiPreserveAllSemiColons, yuiDisableOptimizations)
-	                } else if (".css".equalsIgnoreCase(extension)) {
-	                    val compressor = new CssCompressor(in)
-	                    compressor.compress(out, lineBreaks)
-	                }
-	            } finally {
-	                out.close
-	            }
-	        }
-		} finally {
-			in.close
-		}
-        
-    }
-    None
-  } describedAs("Minify JS and CSS files with yui compressor") 
-  
-  class YuiErrorReporter extends ErrorReporter {
-        def logit(level:Level.Value, message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) = 
-            log.log(level, "%s in %s:%d,%d at %s".format(message, sourceName,line,lineOffset, lineSource))
-            
-        def error(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) {
-            logit(Level.Error, message, sourceName, line,lineSource,lineOffset)
-        }
-
-        def runtimeError(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int):EvaluatorException = {
-            error(message, sourceName, line,lineSource,lineOffset)
-            new EvaluatorException(message, sourceName, line, lineSource, lineOffset)
-        }
-
-        def warning(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) {
-            logit(Level.Warn, message, sourceName, line,lineSource,lineOffset)
-        }
-      
+trait YuiCompressorPlugin extends BasicScalaProject {
+ 
+  trait YuiOptions {
+    val encoding:String = "UTF-8"
+    val nomunge:Boolean = false
+    val jswarn:Boolean = false
+    val preserveAllSemiColons:Boolean = false
+    val disableOptimizations:Boolean = false
+    val lineBreaks:Int = -1
   }
   
-  override def prepareWebappAction = super.prepareWebappAction dependsOn(yuicompressor) 
-  override def extraWebappFiles = (yuiTemp ##) ** "*"
-  override def webappResources = super.webappResources --- yuiSources
-  override def watchPaths = super.watchPaths +++ yuiSources
+  def yuiCompressorTask(
+        product:Path
+        , sources:Iterable[Path]
+        , options:YuiOptions
+  ) = 
+    fileTask(Seq(product) from sources) {
+
+      val errorReporter = new YuiErrorReporter()
+      val inFiles = sources.map( (s)=>{new File(s.toString)} )
+      val maxLastModified = inFiles.foldLeft(-1l)( (r,f) => { 
+            if(f.lastModified > r) f.lastModified
+            else r
+      })
+      val outFile = new File(product.toString)
+
+      if (!outFile.exists || outFile.lastModified < maxLastModified) {
+	if (!outFile.getParentFile.exists && !outFile.getParentFile.mkdirs) {
+	  throw new Exception( "Cannot create resource output directory: " + outFile.getParentFile() )
+	}
+	val out = new OutputStreamWriter(new FileOutputStream(outFile), options.encoding)
+        try {
+
+          for (file <- inFiles) {
+            val in = new InputStreamReader(new FileInputStream(file), options.encoding)
+            try {
+
+	      val fileName = file.toString//.substring(webappPath.toString.length)
+              log.info("Compressing: " + fileName)
+	      val extension = fileName.substring(fileName.lastIndexOf("."))
+
+              if (".js".equalsIgnoreCase(extension)) {
+	        val compressor = new JavaScriptCompressor(in, errorReporter)
+	        compressor.compress(
+                  out
+                  , options.lineBreaks
+                  , !options.nomunge
+                  , options.jswarn
+                  , options.preserveAllSemiColons
+                  , options.disableOptimizations
+                )
+	      } else if (".css".equalsIgnoreCase(extension)) {
+	        val compressor = new CssCompressor(in)
+	        compressor.compress(out, options.lineBreaks)
+	      }
+
+            } finally { in.close }
+          }
+
+        } finally { out.close }
+      }
+
+      None
+    }
+
+  class YuiErrorReporter extends ErrorReporter {
+    def logit(level:Level.Value, message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) = 
+      log.log(level, "%s in %s:%d,%d at %s".format(message, sourceName,line,lineOffset, lineSource))
+            
+    def error(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) =
+      logit(Level.Error, message, sourceName, line,lineSource,lineOffset)
+
+    def runtimeError(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int):EvaluatorException = {
+      error(message, sourceName, line,lineSource,lineOffset)
+      new EvaluatorException(message, sourceName, line, lineSource, lineOffset)
+    }
+
+    def warning(message:String, sourceName:String, line:Int, lineSource:String, lineOffset:Int) =
+      logit(Level.Warn, message, sourceName, line,lineSource,lineOffset) 
+  }
+  
 }
